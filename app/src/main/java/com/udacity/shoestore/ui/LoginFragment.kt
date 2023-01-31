5a81af19a90a80d188be1f086d130ec6c0ba535b
LoremIpsum1512 <huyhoangnt1512@gmail.com>
2023-01-31 13:35:01 +0700

    init

---
+++ app/src/main/java/com/udacity/shoestore/ui/LoginFragment.kt
@@ -0,0 +1,24 @@
+package com.udacity.shoestore.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.databinding.FragmentLoginBinding
+import com.udacity.shoestore.R
+
+class LoginFragment : Fragment() {
+    lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false);
+        return binding.root
+    }
+
+
+}
